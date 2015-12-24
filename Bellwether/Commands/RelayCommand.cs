47a7565e66351965d15734663a87d25d9c3b70b1
Bartosz Ciurla <itsgoodtoride@gmail.com>
2015-12-24 11:15:02 +0100

    init

---
+++ Bellwether/Commands/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Bellwether.Commands
+{
+    public class RelayCommand:ICommand
+    {
+        private readonly Action _action;
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke();
+        }
+    }
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _action;
+        public RelayCommand(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action.Invoke((T)parameter);
+        }
+    }
+}
